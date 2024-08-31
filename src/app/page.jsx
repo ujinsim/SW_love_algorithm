769e1e015fccad603ad8b8fe153f382de684f3e7
‘ujinsimSS’ <syj11243@naver.com>
2024-08-31 11:32:40 +0900

    프로젝트 초기세팅

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-pink-600">
+      tailwind Zustand firebase 설치 후 초기세팅
+    </main>
+  );
+}
